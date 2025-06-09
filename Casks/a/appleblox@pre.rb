062c48b8aa55ecbc9acd678855bb34823f8b4641
okcoder1 <codershub.code@gmail.com>
2025-06-09 17:41:51 +0530

    Update appleblox@pre.rb

--- Casks/a/appleblox@pre.rb
+++ Casks/a/appleblox@pre.rb
@@ -1,8 +1,13 @@
 cask "appleblox@pre" do
   arch arm: "arm64", intel: "x64"
-    
+  on_arm do
+    sha256 "27e4fce883d7db258023d413fffc2a4fab5a6784285a33625597fd31a49210c9"
+  end
+  on_intel do
+    sha256 "30e5809ef65ea585aa6e794b441e731c34e2e21ca5d1c295cbf02e26f0e94b3b"
+  end
+  
   version "0.8.6"
-  sha256 "d66bbf82752dd323ad542902f1f5315d82a3eac7d33cd5076d1ad988e0a61046"
 
   url "https://github.com/AppleBlox/appleblox/releases/download/#{version}/AppleBlox-#{version}_#{arch}.dmg",
     verified: "github.com/AppleBlox/appleblox/"
