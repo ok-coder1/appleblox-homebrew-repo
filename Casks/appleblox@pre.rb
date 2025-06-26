bbb12ad7b952ba211867c76c853ee3b03b5d8352
okcoder1 <codershub.code@gmail.com>
2025-06-26 22:48:29 +0530

    Update appleblox@pre.rb

--- Casks/appleblox@pre.rb
+++ Casks/appleblox@pre.rb
@@ -1,7 +1,7 @@
 cask "appleblox@pre" do
   arch arm: "arm64", intel: "x64"
-  
   version "0.8.6"
+  
   on_arm do
     sha256 "27e4fce883d7db258023d413fffc2a4fab5a6784285a33625597fd31a49210c9"
   end
@@ -12,7 +12,7 @@ cask "appleblox@pre" do
   url "https://github.com/AppleBlox/appleblox/releases/download/#{version}/AppleBlox-#{version}_#{arch}.dmg",
     verified: "github.com/AppleBlox/appleblox/"
   name "AppleBlox"
-  desc "AppleBlox is a Roblox launcher for macOS, inspired by Bloxstrap."
+  desc "Roblox launcher for macOS inspired by Bloxstrap"
   homepage "https://appleblox.com/"
 
   livecheck do
@@ -20,8 +20,8 @@ cask "appleblox@pre" do
     strategy :github_latest
   end
 
-  depends_on macos: :high_sierra
-  depends_on "roblox"
+  depends_on macos: ">= :high_sierra"
+  depends_on cask: "roblox"
 
   app "AppleBlox.app"
 
